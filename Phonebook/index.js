dc8d5cdb3e62792df7bb8a85712dc4095c0db1e0
Nilaksh2003 <nilaksh.ginilytics@gmail.com>
2023-10-25 17:57:28 +0530

    Phonebook backend step2

--- Phonebook/index.js
+++ Phonebook/index.js
@@ -25,6 +25,30 @@ let phoneNumbers=[
 app.get('/api/persons',(request,response)=>{
     response.json(phoneNumbers)
 })
+app.get('/info',(request,response)=>{
+    const months = [
+        'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+        'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+      ];
+    
+      const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    
+      const now = new Date();
+      const year = now.getFullYear();
+      const month = months[now.getMonth()];
+      const dayOfWeek = days[now.getDay()];
+      const day = now.getDate();
+      const hours = now.getHours();
+      const minutes = now.getMinutes();
+      const seconds = now.getSeconds();
+      const timezoneOffset = now.getTimezoneOffset();
+      const timezoneOffsetHours = Math.abs(Math.floor(timezoneOffset / 60));
+      const timezoneOffsetMinutes = Math.abs(timezoneOffset) % 60;
+      const timezoneSign = timezoneOffset < 0 ? '+' : '-';
+    
+      const formattedDate = `${dayOfWeek} ${month} ${day} ${year} ${hours}:${minutes}:${seconds} GMT${timezoneSign}${timezoneOffsetHours.toString().padStart(2, '0')}${timezoneOffsetMinutes.toString().padStart(2, '0')} (Eastern European Standard Time)`;
+    response.send(`<p>Phonebook has info for ${phoneNumbers.length} people <br> ${formattedDate}</p>`)
+})
 const PORT = 3001
 app.listen(PORT,()=>{
     console.log(`Server running on port${PORT}`)
