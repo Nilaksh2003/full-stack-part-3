773c9b73337da4a90530ac36fd9b3eeb5cc10043
Nilaksh2003 <nilaksh.ginilytics@gmail.com>
2023-10-26 10:38:21 +0530

    Phonebook backend step8

--- Phonebook/index.js
+++ Phonebook/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
 const app=express()
-app.use(morgan('tiny'))
+morgan.token('req-body',(req)=>{
+ return  JSON.stringify(req.body)
+})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req-body'))
 app.use(express.json());
 let phoneNumbers=[
     { 
